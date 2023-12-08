22f45f2fc1d3250f61c7de45d40486d87548c00e
DanielMata <danielmataherchiga2001@gmail.com>
2023-12-08 21:05:41 +0100

    estado de seguimiento

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,10 +1,23 @@
 import "./App.css"
 import { TwitterFollowCard } from "./TwitterFollowCard"
 export function App(){
+
+    //const formattedUserName = <span>@danichu_18</span>
+
     return (
-        <>
-            <TwitterFollowCard username="midudev" name="Miguel Angel Duran" isFollowing="null"/>
-            <TwitterFollowCard username="danichu_18" name="Daniel Mata Herchiga" isFollowing="null"/>
-        </>
+        
+        <section className="contenedor-cards">
+            <TwitterFollowCard>
+                Miguel Ángel Durán
+            </TwitterFollowCard>
+            <TwitterFollowCard username="danichu_18">
+                Daniel Mata Herchiga    
+            </TwitterFollowCard>
+        </section>
+        /*
+        <section className="contenedor-cards">
+            <TwitterFollowCard formatedUser={formattedUserName} username="midudev" name="Miguel Angel Duran" isFollowing/>
+            <TwitterFollowCard formatedUser={formattedUserName} username="danichu_18" name="Daniel Mata Herchiga" isFollowing={false}/>
+        </section>*/
     )
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,4 +1,17 @@
-export function TwitterFollowCard({username, name, isFollowing}){
+import {useState} from "react"  
+export function TwitterFollowCard({children, username = "unknown" }){
+    //ESTADO DE SEGUIMINETO - 1 PARAMETRO ES EL ESTADO(FALSE) Y EL SEGUNDO ES PARA CAMBIARLO
+    const [isFollowing, setIsFollowing] = useState(false)
+
+    //MANEJADOR, CAMBIAR ESTADO DE SEGUIMIENTO, LA LLAMADA SE HACE EN EL BUTTON CON UN ONCLICK
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+
+    const text = isFollowing ? "Siguiendo" : "Seguir"
+    const buttonClassName = isFollowing ? "tw-follow-card-btn-seguir is-following" : "tw-follow-card-btn-seguir"
+    
+
     return (
         <article className="tw-follow-card">
             <header className="tw-follow-card-header">
@@ -6,12 +19,14 @@ export function TwitterFollowCard({username, name, isFollowing}){
                     className="tw-follow-card-img"
                     src={`https://unavatar.io/${username}`}/>
                 <div className="tw-follow-card-info">
-                    <strong className="tw-follow-card-nombre">{name}</strong>
+                    <strong>{children}</strong>
                     <span className="tw-follow-card-nombre-usuario">@{username}</span>
                 </div>
             </header>
             <aside >
-                <button className="tw-follow-card-btn-seguir">Seguir</button>
+                <button className={buttonClassName} onClick={handleClick}>
+                    {text}
+                </button>
             </aside>
         </article>
     )
